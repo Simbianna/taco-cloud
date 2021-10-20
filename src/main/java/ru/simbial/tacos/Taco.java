8593839cc53c9fe10b87fc8b463b0784ccb88de4
Anna <annetkiricheva@yandex.ru>
2021-10-20 14:45:14 +0300

    Chapters 1&2 completed. Used: Spring Boot, Been Validation, Thymeleaf.

---
+++ src/main/java/ru/simbial/tacos/Taco.java
@@ -0,0 +1,20 @@
+package ru.simbial.tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<String> ingredients;
+}
